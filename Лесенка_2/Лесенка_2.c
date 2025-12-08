564686d3d17e1cbe029b0472326c7bb6d0fefd81
Mikhael <fanfwriter@mail.ru>
2025-12-08 19:48:51 +0300

    Решение задач

--- Лесенка_2/Лесенка_2.c
+++ Лесенка_2/Лесенка_2.c
@@ -1,3 +1,6 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
 /**
  *
  *   Лесенка 2
@@ -9,8 +12,17 @@
  *   5
  */
 
-int main(int argc, char* argv[])
+int main()
 {
-    
+    setlocale(LC_ALL, "Russian");
+    int n = 0;
+    printf("Enter digit n from 0 to 9: ");
+    scanf("%d", &n);
+
+    for (int k = 1; k <= n; k++) {
+        for (int m = n; m >= k; m--) { printf("%d", m); }
+        printf("\n");
+    }
+
     return 0;
 }
